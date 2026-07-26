aa237692a38d27ad3490c6d4385b8935eceb5d78
codewithtayyab90 <khokhart097@gmail.com>
2026-07-26 23:19:51 +0500

    remove slash

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const authRouter = require('./routes/auths')
 
 connectDB()
 app.use(express.json())
-app.use(cors({origin: "https://e-commerce-tayyab.vercel.app/"}))
+app.use(cors({origin: "https://e-commerce-tayyab.vercel.app"}))
 app.use("/products", productRouter)
 app.use("/", authRouter)
 app.all('/*path', (req, res) => {
